fc6da7b89bf0178bec9789c102df90b62d2a2928
Krish-Dev <jangirkrishnacarbon@gmail.om>
2025-08-22 12:55:36 +0530

    Added table

--- Video4/main.py
+++ Video4/main.py
@@ -12,5 +12,3 @@ st.title("My First Streamlit App")
 st.subheader("Introducing Streamlit in Automate Everything with Python!!")
 st.write("""This is our first web app!!
 Enjoy it!!""")
-
-st.write(df)
