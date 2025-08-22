f64df2f5d6f61bc6df496177039a654018d3f9c5
Krish-Dev <jangirkrishnacarbon@gmail.om>
2025-08-22 13:10:19 +0530

    Added table

--- Video4/main.py
+++ Video4/main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+    "Series_1": [1, 2, 3, 4, 5, 6, 7],
+    "Series_2": [100, 250, 125, 45, 32, 56, 98],
+}
+
+df = pd.DataFrame(data)
 
 st.title("My First Streamlit App")
 st.subheader("Introducing Streamlit in Automate Everything with Python!!")
