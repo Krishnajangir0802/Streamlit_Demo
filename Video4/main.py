de43bf44c639348a24fbd6d06f1de0658ae77461
Krish-Dev <jangirkrishnacarbon@gmail.om>
2025-08-22 12:55:36 +0530

    Created Python Script to deploy web page using streamlit.

---
+++ Video4/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("My First Streamlit App")
+st.subheader("Introducing Streamlit in Automate Everything with Python!!")
+st.write("""This is our first web app!!
+Enjoy it!!""")
